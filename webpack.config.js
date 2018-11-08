c374798b87562b3b94a7d73f3057530be80ca669
Nathan Fiedler <nathanfiedler@fastmail.fm>
2018-11-08 04:48:31 -0800

    Upgrade all dependencies (#37)
    Also fix the webpack config file formatting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: {
     basic: './lib/js/examples/basic/basicEntry.js',
     react: './lib/js/examples/react/reactEntry.js',
-    immutable: './lib/js/examples/immutable/immutableEntry.js',
+    immutable: './lib/js/examples/immutable/immutableEntry.js'
   },
   output: {
-    path: path.join(__dirname, "bundledOutputs"),
-    filename: '[name].js',
-  },
-};
+    path: path.join(__dirname, 'bundledOutputs'),
+    filename: '[name].js'
+  }
+}
